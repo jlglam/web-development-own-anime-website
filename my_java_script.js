c7acfb4b2f24dbf7d687ba44ec96096263532f57
Ruben <rc17.dt@gmail.com>
2015-08-04 23:34:36 -0700

    fixing header gradient color and first attempt to embed youtube video

--- my_java_script.js
+++ my_java_script.js
@@ -11,21 +11,27 @@ var main = function() {
 	});
 	
 	/*dealing with side-menu*/
-	$(".menu-icon").click(function(){
+	$(".menu-icon").click(function(event){
+		event.stopPropagation();
 		$(".menu").animate({
 			left: "0px"
 		}, 200);
 			
 	});	
+
 	$(".close-icon").click(function(){
 		$(".menu").animate({
 			left: "-285px"
 		}, 200);
 			
 	});
+
+	$(document).click(function(){
+		$(".menu").animate({
+			left: "-285px"
+		}, 200);
+	});
 	
-	
-	
-}
+};
 
 $(document).ready(main);
