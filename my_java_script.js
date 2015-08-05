eb99a24ff4beeac5326db5e188d012bcf47e3503
Ruben <rc17.dt@gmail.com>
2015-08-04 19:00:39 -0700

    side menu added

--- my_java_script.js
+++ my_java_script.js
@@ -1,9 +1,31 @@
 
-/*do transition for background color of arrows*/
-$( ".header-container" ).hover(
-function() {
-	$(".carousel-actions").css({"visibility":"visible"}); 
-},
-function(){
-	$(".carousel-actions").css({"visibility":"hidden"});
-});
+var main = function() {
+	/*dealing with arrows visibility and background color change*/
+	/*do transition for background color of arrows*/
+	$( ".header-container" ).hover(
+		function() {
+			$(".carousel-actions").css({"visibility":"visible"}); 
+		},
+		function(){
+			$(".carousel-actions").css({"visibility":"hidden"});
+	});
+	
+	/*dealing with side-menu*/
+	$(".menu-icon").click(function(){
+		$(".menu").animate({
+			left: "0px"
+		}, 200);
+			
+	});	
+	$(".close-icon").click(function(){
+		$(".menu").animate({
+			left: "-285px"
+		}, 200);
+			
+	});
+	
+	
+	
+}
+
+$(document).ready(main);
